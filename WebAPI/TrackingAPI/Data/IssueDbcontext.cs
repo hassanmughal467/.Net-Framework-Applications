1034e82c1a3673ead216d4e127081d667f6307cf
Muhammad Hassan <55318080+hassanmughal467@users.noreply.github.com>
2022-04-19 15:23:31 -0700

    webAPI

---
+++ WebAPI/TrackingAPI/Data/IssueDbcontext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrackingAPI.models;
+
+namespace TrackingAPI.Data
+{
+    //IssueDbcontext inherit with DbContext for interacting
+    public class IssueDbcontext : DbContext
+    {
+        //WeakReference use dboptions this allow setting up options needed by dbcontext like connectionstring
+        public IssueDbcontext(DbContextOptions<IssueDbcontext> options) : base(options)
+        {
+
+
+        }
+        //DbSet is UseExtensions to representation of data in database.It will allow us to maniuplate data from a table issue
+        public DbSet<Issue> Issue
+            { get; set; } 
+
+        //after db context is WeakReference have to register it with dependency injections container this way we can request an instance in the controller
+            
+    }
+}
---
+++ WebAPI/TrackingAPI/models/Issue.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrackingAPI.models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public IssueType Type { get; set; }
+
+        public DateTime Created { get; set; }
+        
+        public DateTime? Completed { get; set; }
+    }
+
+    public enum Priority
+    { low, Medium , high }
+
+    public enum IssueType
+    {
+        Feature, Bug, Documentation
+    }
+}
